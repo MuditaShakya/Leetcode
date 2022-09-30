6c258ccc80d621afec73ac385a105172121517f8
MuditaShakya <103852813+MuditaShakya@users.noreply.github.com>
2022-09-30 22:35:33 +0530

    Time: 2 ms (76.62%), Space: 73.1 MB (76.95%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,20 @@
+class Solution { 
+    public int maxSubArray(int[] a) {
+        int csum=a[0];
+        int osum=a[0];
+        for(int i=1;i<a.length;i++)
+        {  if(csum>0){
+            csum+=a[i];
+        }
+         else {
+             csum=a[i];
+         }
+        
+        if(osum<csum)
+            osum=csum;
+        
+        }
+        return osum;
+        
+    }
+}
