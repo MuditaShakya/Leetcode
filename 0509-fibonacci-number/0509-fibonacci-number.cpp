ce0dfa2e0e283d5898df38fe4baabcb2cd2997a8
Mudita20 <103852813+MuditaShakya@users.noreply.github.com>
2023-01-17 19:15:21 +0530

    Time: 14 ms (41.38%), Space: 6 MB (19.64%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int fib(int n) { int f;
+        if(n==0|| n==1) return n;
+        else{
+             f=fib(n-1)+fib(n-2); 
+            return f;
+            
+        }
+    }
+};
