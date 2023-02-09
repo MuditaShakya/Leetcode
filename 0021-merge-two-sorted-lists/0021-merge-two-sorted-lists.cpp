705b1b6634674460fd52ef4ce70beca080a1db99
Mudita20 <103852813+MuditaShakya@users.noreply.github.com>
2023-02-09 19:25:10 +0530

    Time: 12 ms (39.86%), Space: 14.6 MB (98.88%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -14,7 +14,7 @@ public:
          if(l1==NULL) return l2;
          if(l2==NULL) return l1;
          
-         if(l1->val< l2->val)
+         if(l1->val<=l2->val)
          {
              l1->next=merge(l1->next, l2);
              return l1;
