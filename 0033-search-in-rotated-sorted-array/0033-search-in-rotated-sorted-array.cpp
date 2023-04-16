38c29b9d9bdf958adf934af728e944e59c246823
Mudita20 <103852813+MuditaShakya@users.noreply.github.com>
2023-04-16 20:19:56 +0530

    Time: 10 ms (11.98%), Space: 11.2 MB (44.44%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,10 +1,47 @@
 class Solution {
 public:
-    int search(vector<int>& n, int t) {
-        for(int i=0;i<n.size();i++)
+    int search(vector<int>& nums, int t) {
+        int l=nums.size();
+    int minIdx= findMin(nums);
+      int a=  binary(nums, t, 0, minIdx-1);
+      int b=   binary(nums, t, minIdx, l-1);
+     if(a>=b) return a ;
+        else return b;
+        
+    }
+    int findMin(vector<int>& n) {
+        int N=n.size();
+        int start=0, end=n.size()-1;
+        if(n[end]>=n[0]) return n[0];
+        while(start<=end)
         {
-            if(n[i]==t)
-                return i;
-        } return (-1);
+            int mid=(start+end)/2;
+            int prev=(mid+N-1)%N;
+            int next=(mid+1)%N;
+            
+            if(n[mid]<=n[prev] && n[mid]<=n[next])
+                return(mid);
+            else if(n[0]<=n[mid])
+                start=mid+1;
+            else
+                end=mid-1;
+        }
+        return(-1);
+    }
+    
+    int binary(vector<int>& a, int t, int s, int e)
+    {
+         while(s<=e)
+        {
+            int mid=(s+e)/2;
+        
+            
+            if(a[mid]==t)
+                return(mid);
+            else if(a[mid]<t)
+                s=mid+1;
+            else
+                e=mid-1;
+    } return(-1);
     }
 };
