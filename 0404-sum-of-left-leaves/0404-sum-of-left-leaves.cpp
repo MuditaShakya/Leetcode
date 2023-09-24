59e1022ec266ec2666d645442a14b3fc31cf355c
Mudita20 <103852813+MuditaShakya@users.noreply.github.com>
2023-09-24 23:59:02 +0530

    Time: 8 ms (6.93%), Space: 13.7 MB (5.27%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(root==NULL)
+            return 0;
+         int sum=0;
+      return sumleftleaf(root, sum);
+        
+        
+    }
+    TreeNode* leftleaf(TreeNode* root){
+        if(root==NULL) return NULL;
+        if(root->left==NULL && root->right==NULL)
+            return NULL;
+       // if(root->left==NULL && root->right!=NULL) root=root->right;
+        if(root->left==NULL) return NULL;
+        while(root->left!=NULL)
+        {
+            root=root->left;
+            
+        } if(root->left==NULL && root->right==NULL) 
+            return root;
+        return NULL;
+    }
+    int sumleftleaf(TreeNode* root,int &sum)
+    { if(root==NULL)
+        return 0;
+        TreeNode* t=leftleaf(root);
+     if(t!=NULL){
+        sum=sum+t->val;
+     t->val=0;}
+     
+        sumleftleaf(root->right, sum);
+       sumleftleaf(root->left,sum);
+        return sum;
+    }
+};
