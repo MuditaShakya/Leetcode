5dd35e6b967148e9378489b09dacc9a3bb50c170
Mudita20 <103852813+MuditaShakya@users.noreply.github.com>
2023-10-03 01:52:25 +0530

    Time: 3 ms (60.38%), Space: 13.2 MB (23.89%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -1,20 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    
-    void util(vector<int>& a,TreeNode* root)
-    {
-        if(!root)
-            return;
-        if(root->left==root->right && root->left==NULL)
-            a.push_back(root->val);
-        util(a,root->left);
-        util(a,root->right);
-    }
-    
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int> a1,a2;
-        util(a1,root1);
-        util(a2,root2);
-        return a1==a2;
+        if(root1==NULL || root2==NULL) return false;
+        vector<int> a,b;
+        leaf(root1,a);
+        leaf(root2,b);
+    return(a==b);
+        
+    }
+    void leaf(TreeNode* root, vector<int> &v)
+    {
+        if(root==NULL)
+            return;
+        if(root->left==root->right and root->left==NULL)
+            v.push_back(root->val);
+        leaf(root->left, v);
+        leaf(root->right,v);
+return;
     }
 };
