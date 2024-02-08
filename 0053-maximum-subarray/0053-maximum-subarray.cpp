deb71726f969ef0439721eca2cdbf32147cd33de
Mudita20 <103852813+MuditaShakya@users.noreply.github.com>
2024-02-08 15:02:58 +0530

    Time: 82 ms (61.80%), Space: 70.2 MB (26.00%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& n) {
+        int csum=n[0];
+        int osum=n[0];
+        int i=1;
+        while(i<n.size())
+        {
+            if(csum>0)
+            {
+                csum=csum+n[i];
+            }
+                else
+                    csum=n[i];
+            if(csum>osum)
+                osum=csum;
+            i++;
+        }
+        return osum;
+    }
+};
