e8827ba5ff8ef10fd0279214775887145b6eb79e
Mudita20 <103852813+MuditaShakya@users.noreply.github.com>
2024-02-26 20:03:38 +0530

    Time: 0 ms (100.00%), Space: 12.6 MB (38.33%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& a, vector<int>& b) {
-        sort(a.begin(), a.end());
-         sort(b.begin(), b.end());
-        int n=a.size();
-        int m=b.size();
-        int i=0;
-        int j=0;
-        
+        set<int> s;
         vector<int> v;
-      while(i<n && j<m)
-      {    
-         if(a[i]<b[j]){
-              i++;
-          }
-        else  if(a[i]>b[j])
-          {
-              j++;
-          }
-           else
-          { 
-               if(v.empty() || v.back() !=a[i])
-              v.push_back(a[i]);
-               i++;
-              j++;
-              
-          }
-      } 
-        return v;
+        for(int i=0;i<a.size();i++)
+        {
+            s.insert(a[i]);  //insert a1 elements in set
+        }
+        for(auto it:b){
+            if(s.count(it)){    //check if elements of present in set?
+                v.push_back(it); // store them in vector
+                s.erase(it);  //erase to avoid duplication
+            }
+        }
+            return v;
     }
 };
