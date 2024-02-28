f45988255c89d21b75a297fbe9bbc4070773aa83
Mudita20 <103852813+MuditaShakya@users.noreply.github.com>
2024-02-28 22:50:17 +0530

    Time: 6 ms (99.73%), Space: 11.8 MB (73.55%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int longestCommonSubsequence(string s1, string s2) {
-        vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,0));//2-d vector of size s1 and s2 all initialized to zero
-	 // for i==0
-	for(int j=0;j<=s2.size();j++){
-		dp[0][j] = 0;
-	}
-	// for j==0
-	for(int i=0;i<=s1.size();i++){
-		dp[i][0] = 0;
-	}
-	for(int i=1;i<=s1.size();i++){
-		for(int j=1;j<=s2.size();j++){
-			if(s1[i-1] == s2[j-1]){
-				dp[i][j] = 1 + dp[i-1][j-1];
-			}
-			else{
-				dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-			}
-		}
-	}
-	return dp[s1.size()][s2.size()];
-}
-    
+        int m = s1.size();
+        int n = s2.size();
+        int dp[m + 1][n + 1]; // Initialize dp with dimensions (m+1) x (n+1)
+
+        // Initialize the first row and first column of dp with 0
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = 0;
+        }
+
+        // Populate dp using dynamic programming
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
 };
