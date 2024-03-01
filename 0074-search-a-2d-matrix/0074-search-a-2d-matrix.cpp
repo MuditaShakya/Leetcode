15e54364cec503f9eb0f50fe51729b602aefa5af
Mudita20 <103852813+MuditaShakya@users.noreply.github.com>
2024-03-01 15:50:39 +0530

    Time: 3 ms (69.42%), Space: 12 MB (6.95%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,19 +3,29 @@ public:
     bool searchMatrix(vector<vector<int>>& m, int t) {
         int n=m.size();
         int nn=m[0].size();
-        int r=0;
-        int c=nn-1;
-        while(c>=0 && r<n)
-        {
-            if(t==m[r][c])
-                return true;
-            else if(t<m[r][c]){
-                c--;
-            }
-            else
-                r++;
-            
-        }
+        
+       for(int i=0;i<n;i++)
+       {
+           
+          if(m[i][0] <=t && t<=m[i][nn-1])
+         return binarySearch(m[i], t);
+       }
         return false;
+    }
+    bool binarySearch(vector<int> &a, int t)
+    {
+        int n=a.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(a[mid]==t)
+                return true;
+            else if(a[mid]>t)
+                high=mid-1;
+            else low=mid+1;
+        }
+        return false;      
     }
 };
