06974576e5100f396afb89c986db454ecf3c2c06
Mudita20 <103852813+MuditaShakya@users.noreply.github.com>
2024-03-18 14:51:05 +0530

    Time: 11 ms (99.70%), Space: 31.9 MB (96.08%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -19,11 +19,9 @@ public:
          if(root1==NULL && root2!=NULL)
             return root2;
         
-        
-        TreeNode* root=new TreeNode();
-        root->val=root1->val + root2->val;
-        root->left=mergeTrees(root1->left, root2->left);
-        root->right=mergeTrees(root1->right, root2->right);
-        return root;
+        root1->val=root1->val + root2->val;
+        root1->left=mergeTrees(root1->left, root2->left);
+        root1->right=mergeTrees(root1->right, root2->right);
+        return root1;
     }
 };
