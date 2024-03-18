9447a1a266c6126482aed5e325fcb6f148abe6b7
Mudita20 <103852813+MuditaShakya@users.noreply.github.com>
2024-03-18 14:25:41 +0530

    Time: 32 ms (15.21%), Space: 32.7 MB (32.77%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -12,44 +12,18 @@
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-         if(root1==NULL && root2!=NULL){
-             return root2;
-         }
-         merge(root1, root2);
-         add(root1, root2);
-        return root1;
-    }
-     TreeNode* merge(TreeNode* root1, TreeNode* root2) {
-         if(root1==NULL && root2==NULL)
-             return NULL;
-     if(root1!=NULL && root2!=NULL)
-         root1->val=root1->val+root2->val;
-         if(root1->left!=NULL && root2->left!=NULL)
-             merge(root1->left, root2->left);
-          if(root1->right!=NULL && root2->right!=NULL)
-             merge(root1->right, root2->right);
-         return root1;
-     }
-     TreeNode* add(TreeNode* root1, TreeNode* root2) {
-        if(root1==NULL && root2==NULL)
-            return NULL;
-         if(root1==NULL && root2!=NULL) return root2;
-             if(root1!=NULL && root2==NULL) return root1;
-
+       if(root1==NULL && root2==NULL)
+           return NULL;
+        if(root1!=NULL && root2==NULL)
+            return root1;
+         if(root1==NULL && root2!=NULL)
+            return root2;
         
-         
-         if(root1->left==NULL && root2->left!=NULL)
-            root1->left=root2->left;
-         
-              if(root1->right==NULL && root2->right!=NULL)
-                  root1->right=root2->right;
-         
-          if(root2->left!=NULL)
-             add(root1->left, root2->left);
-           if(root2->right!=NULL)
-         add(root1->right, root2->right);
-         
-         return root1;
-     }
-   
+        
+        TreeNode* root=new TreeNode();
+        root->val=root1->val + root2->val;
+        root->left=mergeTrees(root1->left, root2->left);
+        root->right=mergeTrees(root1->right, root2->right);
+        return root;
+    }
 };
