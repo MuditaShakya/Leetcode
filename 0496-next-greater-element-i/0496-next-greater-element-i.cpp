2b98d86ff44075d247ef92f913d545f560c6dd0c
Mudita20 <103852813+MuditaShakya@users.noreply.github.com>
2024-04-19 00:42:37 +0530

    Time: 12 ms (12.54%), Space: 60.4 MB (5.25%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& n1, vector<int>& n2) {
-        int l1=n1.size(); 
-        int l2=n2.size();
-        vector<int> b;
-        for(int i=0;i<l1;i++)
-        {  int count=0;
-            for(int j=0;j<l2;j++)
-            { 
-              if(n2[j]==n1[i])
-                { 
-                 int k=j+1; 
-                   while(k<l2)
-                    {
-                        if(n2[k]>n2[j]){
-                        b.push_back(n2[k]); count++; break;}
-                         k++;
-                     }    
-                 } } 
-             if(count==0)
-                b.push_back(-1);}
-        
-            
-         return b;
-        
+        vector<int> ans;
+        for(int i=0;i<n1.size();i++)
+        {
+            int ele=n1[i];
+            int t=func(ele, n2);
+            ans.push_back(t);
+        }
+        return ans;
+    }
+    int func(int ele, vector<int> n2)
+    {
+        stack<int> s;
+       int i=n2.size()-1;
+        while(n2[i]!=ele)
+        {
+            s.push(n2[i]);
+            i--;
+        }
+        if(s.empty()==true)
+            return -1;
+        while(!s.empty() && s.top()<=ele)
+        {
+            s.pop();
+        }
+        if(s.empty()==true)
+            return -1;
+        return s.top();
     }
 };
