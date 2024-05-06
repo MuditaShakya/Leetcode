9546cc5ef20904bedffb1fb7d8bcc07d13625b89
Mudita20 <103852813+MuditaShakya@users.noreply.github.com>
2024-05-07 01:36:23 +0530

    Time: 7 ms (48.06%), Space: 16 MB (86.18%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,55 +1,49 @@
 class Solution {
 public:
-    
-    int lowerbound( vector<int>& a, int t)
-    {
-        int l=0; int r=a.size()-1;
-        int ans=-1;
-        int mid=l+(r-l)/2;
-        
-        while(l<=r)
-        {
-            if(a[mid]==t){
-                ans=mid;
-            r=mid-1;
-            }
-            else if(a[mid]> t)
-                r=mid-1;
-            else
-                l=mid+1;
-            
-            mid=l+(r-l)/2;
-        }
-        return ans;
-        
-        
-    }
-    
-      int upperbound( vector<int>& a, int t)
-    {
-        int l=0; int r=a.size()-1;
-        int ans=-1;
-        int mid=l+(r-l)/2;
-        
-        while(l<=r)
-        {
-            if(a[mid]==t){
-                ans=mid;
-            l=mid+1;
-            }
-            else if(a[mid]> t)
-                r=mid-1;
-            else
-                l=mid+1;
-            
-            mid=l+(r-l)/2;
-        }
-        return ans;
-      }
     vector<int> searchRange(vector<int>& a, int t) {
+         int l=0;
+        int r=a.size()-1;
+        int mid;
         vector<int> v;
-        v.push_back(lowerbound(a, t));
-        v.push_back(upperbound(a,t));
+        int ans=-1;
+        while(l<=r)
+        {
+            mid=l+(r-l)/2;
+            if(a[mid]==t)
+            {
+                ans=mid;
+                r=mid-1;
+            }
+               
+            else if(a[mid]<t)
+                l=mid+1;
+            else
+                r=mid-1;
+            
+        }
+        v.push_back(ans);
+        ans=-1;
+        
+           l=0;
+         r=a.size()-1;
+        
+        
+        while(l<=r)
+        {
+            mid=l+(r-l)/2;
+            if(a[mid]==t)
+            {
+                ans=mid;
+             l=mid+1;
+            }
+               
+            else if(a[mid]<t)
+                l=mid+1;
+            else
+                r=mid-1;
+            
+        }
+         v.push_back(ans);
         return v;
     }
 };
