989d62021927288f056193e139fcc3a353d97265
Mudita20 <103852813+MuditaShakya@users.noreply.github.com>
2024-08-02 03:00:24 +0530

    Time: 4 ms (82.84%), Space: 17.5 MB (28.98%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& a) {
+        int c=0;
+        for(auto it: a)
+        {
+            char tens=it[11];
+            char ones=it[12];
+            if(tens>'5' && ones>'0' || tens>'6')
+                c++;
+        }
+        return c;
+    }
+};
